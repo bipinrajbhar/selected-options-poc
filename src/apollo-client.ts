83c552532994ada7e9a17836dc881ccce0437e64
Bipin Rajbhar <brajbhar@rh.com>
2025-07-17 15:46:21 -0700

    Update GraphQL endpoint in Apollo Client configuration to staging URL

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "https://intc.rhnonprod.com/rh-experience-layer-v1-intc/graphql", // GraphQL endpoint
+  uri: "https://stg2.rhnonprod.com/rh-experience-layer-v1-stg2/graphql", // GraphQL endpoint
 });
 
 export const client = new ApolloClient({

f1719e729dd87d726c00dfb53ec821160cc0b34a
Bipin Rajbhar <brajbhar@rh.com>
2025-07-18 13:15:27 -0700

    Comment out unused Link import in ProductGallery component to clean up code.

--- src/pages/ProductGallery.tsx
+++ src/pages/ProductGallery.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./ProductGallery.css";
 
 interface Hit {
